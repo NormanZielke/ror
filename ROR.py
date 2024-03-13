f96fcf7dfe0cba264478d35854463ee5fda15b3f
Norman Zielke <Norman.Zielke@rl-institut.de>
2024-03-13 12:19:13 +0100

    Create dataframe ror_timeseries with standardized dispatch for every region. columns as ags_id

--- ROR.py
+++ ROR.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use("seaborn-v0_8")
 
-# load data in filter by columns of interest
+# load data and filter by columns of interest
 
 ror_data_raw = pd.read_csv("bnetza_mastr_hydro_raw.csv",
                         index_col = 0, sep=",")
@@ -20,7 +20,19 @@ ror_data = ror_data_raw[columns]
 regions = ["Rüdersdorf bei Berlin", "Strausberg", "Erkner", "Grünheide (Mark)",
            "Kiel", "Ingolstadt", "Kassel", "Bocholt", "Zwickau"]
 
-dfs= []
+gemeindeschluessel = {
+    "Rüdersdorf bei Berlin": "12064428",
+    "Strausberg": "12064472",
+    "Erkner": "12067124",
+    "Grünheide (Mark)": "12067201",
+    "Ingolstadt":"09161000",
+    "Kassel": "06611000",
+    "Bocholt": "05554008",
+    "Kiel": "01002000",
+    "Zwickau": "14524330",
+}
+
+dfs = []
 for region in regions:
     dfs.append(ror_data.loc[ror_data["Gemeinde"] == region])
 
@@ -36,6 +48,25 @@ data = {"Bruttoleistung_kW": ror_grouped.values
 }
 df_ror_disp = pd.DataFrame(data,index=ror_grouped.index)
 
-df_ror_disp["estimated_Generation_GWh"] = df_ror_disp["Bruttoleistung"] * full_load_hours /1e3
-df_ror_disp["cf"] = df_ror_disp["estimated_Generation_GWh"].div(df_ror_disp["Bruttoleistung"] * 8760 /1e3)
-df_ror_disp["dispatch_kW"] = df_ror_disp["estimated_Generation_GWh"]*1e3 / 8760
+df_ror_disp["estimated_Generation_MWh"] = df_ror_disp["Bruttoleistung_kW"] * full_load_hours /1e3
+df_ror_disp["cf"] = df_ror_disp["estimated_Generation_MWh"].div(df_ror_disp["Bruttoleistung_kW"] * 8760 /1e3)
+df_ror_disp["dispatch_kW"] = df_ror_disp["estimated_Generation_MWh"]*1e3 / 8760
+df_ror_disp["dispatch_kW_standardize"] = df_ror_disp["dispatch_kW"] / (df_ror_disp["estimated_Generation_MWh"] * 1e3)
+
+
+global date_range
+start_date = "2011-01-01 00:00:00"
+end_date = "2011-12-31 23:00:00"
+date_range = pd.date_range(start=start_date, end=end_date, freq='H')
+
+ror_timeseries = pd.DataFrame(index=date_range,columns=regions)
+
+for column in ror_timeseries:
+    if column in df_ror_disp.index:
+        ror_timeseries[column] = df_ror_disp.loc[column,"dispatch_kW_standardize"]
+
+for column in ror_timeseries:
+    ror_timeseries.rename(columns={column:gemeindeschluessel[column]}, inplace=True)
+
+
+ror_timeseries.to_csv("timeseries/ror_feedin_timeseries.csv")
